0c793af54eff4f60b40eac72236dba53112d8fc5
Ekin Akkaya <ekoeko0654@gmail.com>
2024-11-06 23:55:06 +0300

    routes

---
+++ src/NewNote.tsx
@@ -0,0 +1,10 @@
+import NoteForm from "./NoteForm";
+
+export function NewNote() {
+  return (
+    <div>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </div>
+  );
+}
---
+++ src/NoteForm.tsx
@@ -0,0 +1,29 @@
+import { Col, Form, Row, Stack } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
+
+const NoteForm = () => {
+  return (
+    <>
+      <Form>
+        <Stack gap={3}>
+          <Row>
+            <Col>
+              <Form.Group controlId={"title"}>
+                <Form.Label>Title</Form.Label>
+                <Form.Control required />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId={"tags"}>
+                <Form.Label>Tags</Form.Label>
+                <CreatableReactSelect isMulti />
+              </Form.Group>
+            </Col>
+          </Row>
+        </Stack>
+      </Form>
+    </>
+  );
+}
+
+export default NoteForm;
