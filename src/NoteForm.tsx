d9626a0a6995a91e9683df77d9185701a2725d51
Ekin Akkaya <ekoeko0654@gmail.com>
2024-11-08 23:27:16 +0300

    edit page shows note data and tags

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -9,13 +9,20 @@ type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
-const NoteForm = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
+const NoteForm = ({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFormProps) => {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
-  const navigate = useNavigate()
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+  const navigate = useNavigate();
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -26,7 +33,7 @@ const NoteForm = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
       tags: selectedTags,
     });
 
-    navigate("..")
+    navigate("..");
   };
 
   return (
@@ -37,7 +44,7 @@ const NoteForm = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
             <Col>
               <Form.Group controlId={"title"}>
                 <Form.Label>Title</Form.Label>
-                <Form.Control ref={titleRef} required />
+                <Form.Control ref={titleRef} required defaultValue={title} />
               </Form.Group>
             </Col>
             <Col>
@@ -56,8 +63,8 @@ const NoteForm = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
                       value: tag.id,
                     };
                   })}
-                  options={availableTags.map(tag => {
-                    return {label: tag.label, value: tag.id}
+                  options={availableTags.map((tag) => {
+                    return { label: tag.label, value: tag.id };
                   })}
                   onChange={(tags) => {
                     setSelectedTags(
@@ -81,6 +88,7 @@ const NoteForm = ({ onSubmit, onAddTag, availableTags }: NoteFormProps) => {
                 as={"textarea"}
                 ref={markdownRef}
                 rows={15}
+                defaultValue={markdown}
               />
             </Form.Group>
           </Row>
